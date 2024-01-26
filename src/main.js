5c0ad4596235295ff8d768f130af6871eb324f89
hortencia <hortenciafvs@gmail.com>
2024-01-26 15:50:50 -0300

    renderizar cards,section html,css,view,main

--- src/main.js
+++ src/main.js
@@ -5,3 +5,5 @@ import data from './data/dataset.js';
 
 console.log(example, renderItems(data), data);
 
+const cardHobby = document.querySelector("#root");
+cardHobby.appendChild(renderItems(data));
