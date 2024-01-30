ec644918b3ba202531e0f241ab1ff97eb6948c63
Jessica Ramos <jessramos1311@gmail.com>
2024-01-30 14:38:46 -0300

    incluí os microdados no view.js e iniciamos o css

--- src/view.js
+++ src/view.js
@@ -4,13 +4,16 @@ export const renderItems = (data) => {
   
   data.forEach((item) => {
     cardHobby.innerHTML+=`
-    <li itemscope itemtype="containerLi" class="containerLi"> 
+    <li itemscope itemtype="http://https://schema.org/HobbyShop" class="containerLi"> 
       <dl itemscope itemtype="#hobby">
-        <dt> <img src="${item.imageUrl}" alt="imagem do card" itemprop="${item.name}" class="imagem_card"  /> </dt>
-        <dt> Nome: </dt>  <dd itemprop="name"> ${item.name}</dd>
-        <dt> Valor: </dt>  <dd itemprop="custoParaIniciar"> ${item.facts.custoParaIniciar}</dd>
-      </dl>
-    
+        <dt><img src="${item.imageUrl}" alt="imagem do card" itemprop="${item.name}" class="imagem_card"  /></dt>
+        <div class="textoCard">
+        <dt>Nome:</dt><dd itemprop="name"> ${item.name}</dd>
+        <dt>Valor:</dt><dd itemprop="priceRange"> ${item.facts.custoParaIniciar}</dd>
+        <dt>Categoria:</dt><dd itemprop="keywords"> ${item.facts.categoriaHobby}</dd>
+        <dt>Material necessário:</dt><dd itemprop="description"> ${item.facts.materialNecessario}</dd>
+        </div>
+        </dl>
     </li>
     `;
   })
