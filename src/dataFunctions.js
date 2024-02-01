3fc94f80f0085d20f666112ce7f9fc4c41fc5daa
Marcela <maarcelaferraaz@gmail.com>
2024-02-01 12:39:04 -0300

    filtragem dos dados

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,7 +1,15 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
-export const example = () => {
-  return 'example';
+export const filterBy = (data, filterBy, value) =>{
+const filteredData = data.filter(item => {
+  if(filterBy in item){
+    return item[filterBy].includes(value);
+  }else if('facts.categoriaHobby' in item && filterBy in item.facts.categoriaHobby){
+    return item.facts.categoriaHobby[filterBy] ===value;
+  }
+  return false;
+});
+  return filteredData;
 };
 
 export const anotherExample = () => {
